0ab06c565ff45d707482a3a95b156cc0c8bf59c2
Shakya Dissanayake <info.dev.shakya@gmail.com>
2023-12-16 16:37:06 +0530

    feat: added RR scheduling algorithm

---
+++ Algorithms/RoundRobinScheduler.java
@@ -0,0 +1,30 @@
+package Algorithms;
+
+import java.util.Queue;
+import Intefaces.SchedulingAlgorithm;
+import Models.Process;
+
+class RoundRobinScheduler implements SchedulingAlgorithm {
+    private final MultilevelQueueScheduler scheduler;
+
+    public RoundRobinScheduler(MultilevelQueueScheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+
+    @Override
+    public void execute(Queue<Process> queue, int quantum) {
+        Process process = queue.poll();
+        System.out.println("Executing " + process);
+        assert process != null;
+        process.waitingTime += scheduler.getTime() - process.arrivalTime;
+        if (process.burstTime > quantum) {
+            process.burstTime -= quantum;
+            scheduler.updateTime(quantum);  // Update time using scheduler instance
+            queue.add(process);
+        } else {
+            scheduler.updateTime(process.burstTime);  // Update time using scheduler instance
+            process.burstTime = 0;
+            System.out.println("Process " + process.processId + " completed.");
+        }
+    }
+}
